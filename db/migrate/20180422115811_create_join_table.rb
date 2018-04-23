df4f339ae81a91c3dbea9098c4ebbfc89ac74811
Alexander Tischhart <atischhart.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 09:41:23 +0200

    alter relationship table

--- db/migrate/20180422115811_create_join_table.rb
+++ db/migrate/20180422115811_create_join_table.rb
@@ -3,6 +3,7 @@ class CreateJoinTable < ActiveRecord::Migration[5.1]
     create_join_table :users, :tasks do |t|
       t.index [:user_id, :task_id]
       t.index [:task_id, :user_id]
+      t.boolean :confirmed
     end
   end
 end
