f739ad7f36463c9988190900bfb9e57a6bdd746a
Christoph Mayr <23389130+christoph-clouds@users.noreply.github.com>
2018-04-24 15:54:29 +0200

    feat: users can now apply for a task(finally)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users
-  resources :tasks
+  resources :tasks do
+    member do
+      post 'add_user'
+    end
+  end
   #root 'static_pages#home'
 	root 'sessions#create'
   resources :roles
@@ -10,5 +14,5 @@ Rails.application.routes.draw do
   post '/login',  to: 'sessions#create'
   get  '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  '/addUser', to: 'tasks#addUser'
+  # get  '/add_user', to: 'tasks#addUser'
 end
