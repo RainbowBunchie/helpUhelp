e028098850620ef4de16236c56bb2beb8e450a24
Alexander Tischhart <atischhart.mmt-b2016@fh-salzburg.ac.at>
2018-04-25 16:38:24 +0200

    i18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   belongs_to :role
   has_secure_password
   validates :password, presence: true, allow_nil: true
-  validates :first_name, presence: {:message => "Bitte geben Sie Ihren Vornamen ein!"}, length: { minimum: 2 } 
-  validates :last_name, presence: {:message => "Bitte geben Sie Ihren Nachnamen ein!"}, length: { minimum: 2 } 
-  validates :email, presence: {:message => "Bitte geben Sie Ihre E-Mail-Adresse ein!"}, uniqueness: true, length: { minimum: 5 } 
-  validates :telephone, presence: {:message => "Bitte geben Sie Ihre Telefonnummer ein!"}, length: { minimum: 5 } 
+  validates :first_name, presence: true, length: { minimum: 2 } 
+  validates :last_name, presence: true, length: { minimum: 2 } 
+  validates :email, presence: true, uniqueness: true, length: { minimum: 5 } 
+  validates :telephone, presence: true, length: { minimum: 5 } 
 end
