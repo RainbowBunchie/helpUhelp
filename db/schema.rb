34998f60f7678ee80fe4b19184823595cb3addf3
Denise Buder <d.buder@sbg.at>
2018-04-25 14:18:40 +0200

    status added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425093345) do
+ActiveRecord::Schema.define(version: 20180425115514) do
 
   create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
