5f64b98aa72b3ba57e5d02fe7d2bec335a8f9925
Alexander Tischhart <atischhart.mmt-b2016@fh-salzburg.ac.at>
2018-04-26 14:16:26 +0200

    dependencies fix

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  has_many :status_task_users, dependent: :destroy
+
   validates :description, presence: true, length: { minimum: 30 }
   validates :title, presence: true, length: { maximum: 20 }
   validates :place, presence: true, length: { minimum: 2 }
