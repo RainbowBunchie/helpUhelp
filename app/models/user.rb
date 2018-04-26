61e5d6785e87723801c08f05e7274b7da18a23fe
Alexander Tischhart <atischhart.mmt-b2016@fh-salzburg.ac.at>
2018-04-26 13:35:32 +0200

    prepare for conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :tasks
-  accepts_nested_attributes_for :tasks
   belongs_to :role
   has_secure_password
   validates :password, presence: true, allow_nil: true
