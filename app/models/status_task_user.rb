4f841224e5dcb76bd5d35b6c739d3996de8f4e74
Denise Buder <d.buder@sbg.at>
2018-05-15 01:09:25 +0200

    refactor: Task Model & Controller: Scope händelt DB-Abfragen

--- app/models/status_task_user.rb
+++ app/models/status_task_user.rb
@@ -3,7 +3,7 @@ class StatusTaskUser < ApplicationRecord
   belongs_to :task
   belongs_to :status
 
-  scope :pendig, -> { where(status_id: 1) }
-  scope :declined, -> { where(status_id: 2) }
-  scope :confirmed, -> { where(status_id: 3) }
+  # scope :pending, -> { where(status_id: 1) }
+  # scope :declined, -> { where(status_id: 2) }
+  # scope :confirmed, -> { where(status_id: 3) }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,35 @@
 class Task < ApplicationRecord
   has_many :status_task_users, dependent: :destroy
+  has_many :users, through: :status_task_users
+
+  scope :future, -> {where("date >= ?", Date.today)}
+  scope :past, -> {where("date < ?", Date.today)}
 
 
-#  scope :future ....
+  scope :pending_future, -> {Task
+    .joins("INNER JOIN status_task_users ON status_task_users.task_id = tasks.id")
+    .where("status_id = 1")
+    .order(time: :desc)}
+
+  scope :confirmed, ->{ Task
+    .order(time: :asc)
+    .joins("INNER JOIN status_task_users ON status_task_users.task_id = tasks.id")
+    .where("status_id = 3")}
+
+ scope :not_pending_and_no_application_future, ->{ Task.future.order(time: :desc) - Task.confirmed - Task.pending_future }
+
+ scope :user_assigned, -> (user) {Task
+   .order(time: :desc)
+   .joins("INNER JOIN status_task_users ON status_task_users.task_id = tasks.id")
+   .where("user_id = ? AND status_id = 3", user.id)
+   }
+
+  scope :user_pending, -> (user) {Task
+   .joins("INNER JOIN status_task_users ON status_task_users.task_id = tasks.id")
+   .where("user_id = ? AND status_id = 1", user.id)
+   .order(time: :desc)}
+
+  scope :user_open_future, -> (user){ Task.future.order(time: :desc) - Task.confirmed - Task.user_pending(user)}
 
 
   validates :description, presence: true, length: { minimum: 30 }
